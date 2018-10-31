9da9a69b5895e3e16e6cc30a1854656cc2d54af1
aaltopiiri <aaltopiiri@outlook.com>
2018-10-31 12:45:03 +0200

    Multiple changes

---
+++ src/index.php
@@ -0,0 +1,31 @@
+<?php
+
+$value = "World";
+$servername = "mysql";
+$username = "myuser";
+$password = "secret";
+
+try {
+    $db = new PDO("mysql:host=$servername;dbname=mydb", $username, $password);
+    // set the PDO error mode to exception
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully"; 
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+
+
+$databaseTest = ($db->query('SELECT * FROM dockerSample'))->fetchAll(PDO::FETCH_OBJ);
+
+?>
+
+<html>
+    <body>
+        <h1>Hello, <?=$value?>!</h1>
+        <?php foreach($databaseTest as $row):?>
+            <p>Hello, <?=$row->name?></p>
+        <?php endforeach;?>
+    </body>
+</html>
